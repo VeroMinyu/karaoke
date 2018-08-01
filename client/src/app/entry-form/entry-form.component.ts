2d8c4a447d2696bd135fce0406a31568153e81d0
Minyu Li <minyuli1997@gmail.com>
2018-08-01 10:05:36 +0200

    m

---
+++ client/src/app/entry-form/entry-form.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { SongsService } from '../../services/songs.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-entry-form',
+  templateUrl: './entry-form.component.html',
+  styleUrls: ['./entry-form.component.css'],
+  providers: [SongsService]
+})
+export class EntryFormComponent implements OnInit {
+  @Output() onChange = new EventEmitter();
+  constructor(public ss: SongsService, public router: Router) { }
+
+  ngOnInit() {
+  }
+
+  submitForm(form){
+    this.ss.addSong({title: form.value.title, artist: form.value.artist, youtube_url: form.value.youtube_url, lyrics: form.value.lyrics})
+    .subscribe(()=>{
+      this.onChange.emit();
+    })
+  }
+
+}
