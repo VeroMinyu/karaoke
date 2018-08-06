73a1f21dee517134e63ba2c2319ea91d5eb20b50
Verónica Reina <veroreinah@gmail.com>
2018-08-06 17:05:41 +0200

    Performances list

--- client/src/app/performances/performances.component.ts
+++ client/src/app/performances/performances.component.ts
@@ -4,7 +4,10 @@ import { PerformanceService } from '../../services/performances.service';
 @Component({
   selector: 'app-performances',
   templateUrl: './performances.component.html',
-  styleUrls: ['./performances.component.css'],
+  styleUrls: [
+    './performances.component.css',
+    '../../../node_modules/@fortawesome/fontawesome-free/css/all.min.css'
+  ],
   providers: [PerformanceService]
 })
 export class PerformancesComponent implements OnInit {
